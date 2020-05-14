5c473528bc486c5a9c7f57cb194524310be85ce9
Charity Parks <tuppercharity@gmail.com>
2020-05-13 20:50:27 -0400

    Done.

---
+++ build/precache-manifest.d33b4af9e8ea98c082ae64e34822c4ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c6fd839ff42920e7727",
+    "url": "/static/css/main.b92cc172.chunk.css"
+  },
+  {
+    "revision": "7c6fd839ff42920e7727",
+    "url": "/static/js/main.79af3dbd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "a1701549dde8d55409e11714aef286dc",
+    "url": "/index.html"
+  }
+];
